45d0e60e07685e53ec2c050a0c0005aecff6ac99
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-31 09:48:35 -0500

    Initial setup and dependencies

---
+++ app/src/main/java/com/devlab74/blogx/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.devlab74.blogx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devlab74.blogx.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
