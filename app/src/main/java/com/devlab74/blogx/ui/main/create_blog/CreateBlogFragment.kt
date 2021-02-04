9c64921595d7cd6b045ca241b91df665ba3ff804
dsimanushkin <dsimanushkin74@gmail.com>
2021-02-04 13:12:21 -0500

    Added functionality to select and crop images from gallery

--- app/src/main/java/com/devlab74/blogx/ui/main/create_blog/CreateBlogFragment.kt
+++ app/src/main/java/com/devlab74/blogx/ui/main/create_blog/CreateBlogFragment.kt
@@ -1,10 +1,21 @@
 package com.devlab74.blogx.ui.main.create_blog
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.devlab74.blogx.R
 import com.devlab74.blogx.databinding.FragmentCreateBlogBinding
+import com.devlab74.blogx.ui.*
+import com.devlab74.blogx.util.Constants.Companion.GALLERY_REQUEST_CODE
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import timber.log.Timber
 
 class CreateBlogFragment: BaseCreateBlogFragment() {
     private var _binding: FragmentCreateBlogBinding? = null
@@ -21,6 +32,126 @@ class CreateBlogFragment: BaseCreateBlogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.blogImage.setOnClickListener {
+            if (stateChangeListener.isStoragePermissionGranted()) {
+                pickFromGallery()
+            }
+        }
+        binding.updateTextview.setOnClickListener {
+            if (stateChangeListener.isStoragePermissionGranted()) {
+                pickFromGallery()
+            }
+        }
+
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            stateChangeListener.onDataStateChange(dataState)
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+            viewState.blogFields.let { newBlogFields ->
+                setBlogProperties(
+                    newBlogFields.newBlogTitle,
+                    newBlogFields.newBlogBody,
+                    newBlogFields.newImageUri
+                )
+            }
+        })
+    }
+
+    private fun setBlogProperties(
+        title: String?,
+        body: String?,
+        image: Uri?
+    ) {
+        image?.let {
+            requestManager
+                .load(image)
+                .into(binding.blogImage)
+        }?: setDefaultImage()
+
+        binding.blogTitle.setText(title)
+        binding.blogBody.setText(body)
+    }
+
+    private fun setDefaultImage() {
+        requestManager
+            .load(R.drawable.default_image)
+            .into(binding.blogImage)
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeType = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
+    private fun launchImageCrop(uri: Uri?) {
+        context?.let {
+            CropImage.activity(uri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(it, this)
+        }
+    }
+
+    private fun showErrorDialog(errorMessage: String) {
+        stateChangeListener.onDataStateChange(
+            DataState(
+                Event(
+                    StateError(
+                        Response(
+                            errorMessage,
+                            ResponseType.Dialog()
+                        )
+                    )
+                ),
+                Loading(false),
+                Data(Event.dataEvent(null), null)
+            )
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when(requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    data?.data?.let { uri ->
+                        launchImageCrop(uri)
+                    }?: showErrorDialog(getString(R.string.error_something_wrong_with_image))
+                }
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    Timber.d("CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE")
+                    val result = CropImage.getActivityResult(data)
+                    val resultUri = result.uri
+                    Timber.d("CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE: uri: $resultUri")
+                    viewModel.setNewBlogFields(
+                        title = null,
+                        body = null,
+                        uri = resultUri
+                    )
+                }
+                CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE -> {
+                    showErrorDialog(getString(R.string.error_something_wrong_with_image))
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.setNewBlogFields(
+            binding.blogTitle.text.toString(),
+            binding.blogBody.text.toString(),
+            null
+        )
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/devlab74/blogx/util/Constants.kt
+++ app/src/main/java/com/devlab74/blogx/util/Constants.kt
@@ -11,5 +11,9 @@ class Constants {
         const val NETWORK_TIMEOUT = 3000L
         const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
         const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
+
+        const val GALLERY_REQUEST_CODE = 201
+        const val PERMISSIONS_REQUEST_READ_STORAGE = 301
+        const val CROP_IMAGE_INTENT_CODE = 401
     }
 }
