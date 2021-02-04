f9aeb6773fbba8fa493b2a7c1aad8ba8e493be87
dsimanushkin <dsimanushkin74@gmail.com>
2021-02-04 08:35:47 -0500

    Added SearchView and functionality for Swipe Refresh

--- app/src/main/java/com/devlab74/blogx/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/devlab74/blogx/ui/main/blog/BlogFragment.kt
@@ -1,13 +1,18 @@
 package com.devlab74.blogx.ui.main.blog
 
+import android.app.SearchManager
+import android.content.Context.SEARCH_SERVICE
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.RequestManager
 import com.devlab74.blogx.R
 import com.devlab74.blogx.databinding.FragmentBlogBinding
@@ -22,12 +27,15 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class BlogFragment : BaseBlogFragment(),
-    BlogListAdapter.Interaction
+    BlogListAdapter.Interaction,
+    SwipeRefreshLayout.OnRefreshListener
 {
     private var _binding: FragmentBlogBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var recyclerAdapter: BlogListAdapter
+
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,8 +48,9 @@ class BlogFragment : BaseBlogFragment(),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
         setHasOptionsMenu(true)
+        binding.swipeRefresh.setOnRefreshListener(this)
 
         initRecyclerView()
         subscribeObservers()
@@ -106,6 +115,59 @@ class BlogFragment : BaseBlogFragment(),
             })
             adapter = recyclerAdapter
         }
+    }
+
+    private fun initSearchView(menu: Menu) {
+        activity?.apply {
+            val searchManager: SearchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+            searchView = menu.findItem(R.id.action_search).actionView as SearchView
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            searchView.maxWidth = Integer.MAX_VALUE
+            searchView.setIconifiedByDefault(true)
+            searchView.isSubmitButtonEnabled = true
+        }
+
+        // Case 1: Enter on keyboard
+        val searchPlate = searchView.findViewById(R.id.search_src_text) as EditText
+        searchPlate.setOnEditorActionListener {v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED || actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val searchQuery = v.text.toString()
+                viewModel.setQuery(searchQuery).let {
+                    onBlogSearchOrFilter()
+                }
+                Timber.e("SearchView: (keyboard or arrow) executing search... $searchQuery")
+            }
+            true
+        }
+
+        // Case 2: Search button clicked in toolbar
+        (searchView.findViewById(R.id.search_go_btn) as View).setOnClickListener {
+            val searchQuery = searchPlate.text.toString()
+            Timber.e("SearchView: (button) executing search... $searchQuery")
+            onBlogSearchOrFilter()
+        }
+    }
+
+    private fun onBlogSearchOrFilter() {
+        viewModel.loadFirstPage().let {
+            resetUI()
+        }
+    }
+
+    private fun resetUI() {
+        binding.blogPostRecyclerview.smoothScrollToPosition(0)
+        stateChangeListener.hideSoftKeyboard()
+        binding.focusableView.requestFocus()
+    }
+
+    override fun onRefresh() {
+        onBlogSearchOrFilter()
+        binding.swipeRefresh.isRefreshing = false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        initSearchView(menu)
     }
 
     override fun onItemSelected(position: Int, item: BlogPost) {
